4f903668d325e5e2e9569d9a1c5a5046c3ad8de7
LinCong <liner.416@gmail.com>
2020-06-20 08:06:27 -0700

    sequelize

--- TODO-DEMO/src/app.js
+++ TODO-DEMO/src/app.js
@@ -1,35 +1,64 @@
 const express = require('express');
 const app = express();
+const models = require('../db/models')
 
 // all err http status == 500
 /** get task list **/
 app.get('/list/:status/:page',async (req,res,next) => {
+    let {status,page} = req.params;
+    let limit = 10;
+    let offset = (page - 1) * limit;
+    let list = await models.Todo.findAndCountAll({
+        where:{
+            status
+        },
+        offset,
+        limit
+    })
     res.json({
-        list:[]
+        list
     })
 })
 
 /** build a todo **/
 app.post('/create',async (req,res,next)=>{
-    let {name,deadline,content} = req.body;
-    res.json({
-        todo:{},
-        name,
-        deadline,
-        content
-    })
+    try{
+        let {name,deadline,content} = req.body;
+        let todo = await models.Todo.create({
+            name,
+            deadline,
+            content
+        })
+        res.json({
+            todo
+        })
+    }catch (error) {
+        next(error)
+    }
 })
 
 /** update or delete a todo **/
 app.post('/update',async (req,res,next)=> {
-    let {name, deadline, content,id} = req.body;
-    res.json({
-        todo: {},
-        name,
-        deadline,
-        content,
-        id
-    })
+    try{
+        let {name, deadline, content,id} = req.body;
+        let todo = await models.Todo.findOne({
+            where:{
+                id
+            }
+        })
+        if (todo && status != todo.status) {
+            todo = await todo.update(
+                name,
+                deadline,
+                content
+            )
+        }
+        res.json({
+            todo
+        })
+    }catch (error) {
+        next(error)
+    }
 })
 
 app.use((err,req,res,next) => {
